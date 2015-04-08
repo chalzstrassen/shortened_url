12e4c0cf4640b9f386bc87411d3e69b098283e5d
Charles+Dylan <Charles+Dylan@appacademy.io>
2015-04-08 14:27:23 -0400

    Create user model.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+URLShortener::Application.config.secret_key_base = '0cfce04d85f5599877d315baba608b17057edea6310e5e405d067a88871dbe0a612c227f24a34e61464d21016409f4092089e51a3bdd1d16c964b65d161b66e9'
