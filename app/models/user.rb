48a39f3de418c34b909fff668a43ad20c7d4bfb8
Charles+Dylan <Charles+Dylan@appacademy.io>
2015-04-08 15:34:00 -0400

    Add associations for visited urls.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ActiveRecord::Base
     class_name: "ShortenedUrl",
     foreign_key: :user_id,
     primary_key: :id
+
+  has_many :visits,
+    class_name: "Visit",
+    foreign_key: :user_id,
+    primary_key: :id
+
+  has_many :visited_urls, through: :visits, source: :shortened_url
 end
